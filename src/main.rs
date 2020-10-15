465d0497b176f8d97c2b58320577e9d7abb9d352
Brandon Slinkard <brandon.slinkard@nelnet.net>
2020-10-15 17:37:43 -0500

    Setting up basic cli boilerplate with the clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+#[macro_use]
+extern crate clap;
+extern crate toml;
+
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(
+            Arg::with_name("file")
+                .short("f")
+                .long("file")
+                .value_name("TOML_FILE")
+                .help("TOML file to read")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("PATTERN")
+                .help("Field to read from the TOML file")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+
+    // Gets a value for config if supplied by user, or defaults to "default.conf"
+    let config = matches.value_of("file").unwrap();
+    println!("Value for config: {}", config);
 }
