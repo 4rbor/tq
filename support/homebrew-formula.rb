24a83761e2acbb26436906905defb0c538901ca8
Brandon Slinkard <brandon.slinkard@nelnet.net>
2020-10-19 16:03:55 -0500

    Start creating release workflow

---
+++ support/homebrew-formula.rb
@@ -0,0 +1,11 @@
+class Tq < Formula
+  desc "Command line TOML Processor"
+  homepage "https://github.com/4rbor/tq"
+  url "{{{tarballUrl}}}"
+  version "{{version}}"
+  sha256 "{{sha256}}"
+
+  def install
+    bin.install "tq"
+  end
+end
