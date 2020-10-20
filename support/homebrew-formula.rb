ece4de75829356adf58f07a139722e32c58bb4cc
Brandon Slinkard <brandon.slinkard@nelnet.net>
2020-10-20 13:24:02 -0500

    underscore

--- support/homebrew-formula.rb
+++ support/homebrew-formula.rb
@@ -1,9 +1,9 @@
 class Tq < Formula
   desc "Command line TOML Processor"
   homepage "https://github.com/4rbor/tq"
-  url "%%tarball%%"
-  version "%%version%%"
-  sha256 "%%sha256%%"
+  url "_tarball_"
+  version "_version_"
+  sha256 "_sha256_"
 
   def install
     bin.install "tq"
