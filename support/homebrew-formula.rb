71a3b59e95f599c1f62b419f5842b39c4f83ba66
Brandon Slinkard <brandon.slinkard@nelnet.net>
2020-10-20 15:34:32 -0500

    Start preparing for macOs vs linux distributions

--- support/homebrew-formula.rb
+++ support/homebrew-formula.rb
@@ -1,9 +1,15 @@
 class Tq < Formula
   desc "Command line TOML Processor"
   homepage "https://github.com/4rbor/tq"
-  url "_tarball_"
   version "_version_"
-  sha256 "_sha256_"
+
+  if OS.mac?
+    url "_tarball_"
+    sha256 "_sha256_"
+  elsif OS.linux?
+    url "_linux_tarball_"
+    sha256 "_linux_sha256_"
+  end
 
   def install
     bin.install "tq"
